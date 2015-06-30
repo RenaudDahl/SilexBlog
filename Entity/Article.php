ca7326a046cf9ea8da2dd2f3614e93933ebca703
Renaud <rdahl@keley-live.com>
2015-06-30 15:33:19 +0200

    Create project with routes and controllers

---
+++ Entity/Article.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: renaud
+ * Date: 30/06/15
+ * Time: 10:33
+ */
+
+namespace SilexBlog\Entity;
+
+class Article {
+    protected $id;
+    protected $title;
+    protected $content;
+
+    function __construct()
+    {
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     * @return $this
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     * @return $this
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param mixed $content
+     * @return $this
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+
+}
---
+++ web/index.php
@@ -0,0 +1,110 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use SilexBlog\Entity\Article;
+use Silex\Provider\FormServiceProvider;
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$app->register(new FormServiceProvider());
+$app->register(new Silex\Provider\TranslationServiceProvider(), array(
+    'locale_fallbacks' => array('en'),
+));
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver'   => 'pdo_sqlite',
+        'path'     => __DIR__.'/app.db',
+    ),
+));
+
+$articles = array(
+   1 => array(
+    'title'     => 'Using Silex',
+    'content'   => '...',
+)
+);
+
+
+
+$app->get('/blog', function () use ($articles) {
+    $output = '';
+    foreach($articles as $post){
+        $output .= $post['title'];
+        $output .= '<br />';
+    }
+
+    return $output;
+});
+
+$app->get('/blog/{article}', function (Article $article) use ($articles, $app) {
+    if (!isset($article)){
+        $app->abort(404, "L'article que vous recherchez n'existe pas.");
+    }
+
+    $post = $article;
+
+    return "<h1>{$post['title']}</h1>"."<p>{$post['content']}</p>";
+})->convert('article', 'converter.article:convert');
+
+$app->post('/admin/create', function(Request $request) use ($app) {
+
+    $article = new Article();
+
+    $form = $app['form.factory']->createBuilder('form', $article)
+        ->add('title', 'text')
+        ->add('content', 'textarea')
+        ->add('save', 'submit')
+        ->getForm();
+
+    $form->handleRequest($request);
+    if ($form->isValid()) {
+        $app['repository.article']->save($article);
+        $message = "L'article a bien été créé !";
+        $app['session']->getFlashBag()->add('success', $message);
+
+    }
+
+    $data = array(
+        'form' => $form->createView(),
+        'title' => "Ecrire un nouvel article",
+    );
+
+    return $app['twig']->render('create.html.twig', $data);
+
+});
+
+$app->post('/admin/update/{article}', function(Request $request, Article $article) use($app) {
+
+    $form = $app['form.factory']->createBuilder('form', $article)
+        ->add('title', 'text')
+        ->add('content', 'textarea')
+        ->add('save', 'submit')
+        ->getForm();
+
+    $form->handleRequest($request);
+    if ($form->isValid()) {
+        $app['repository.article']->save($article);
+        $message = "L'article a bien été modifié !";
+        $app['session']->getFlashBag()->add('success', $message);
+
+    }
+
+    $data = array(
+        'form' => $form->createView(),
+        'title' => "Modifier un article",
+    );
+
+    return $app['twig']->render('update.html.twig', $data);
+
+})->convert('article', 'converter.article:convert');
+
+$app->post('/admin/delete/{article}', function(Request $request, Article $article) {
+
+    return "L'article a bien été supprimé !";
+})->convert('article', 'converter.article:convert');
+
+
+
+$app->run();
